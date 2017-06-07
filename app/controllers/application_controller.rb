e035abf63f61ed8295ce03649a22a8b805831118
Farooq CH <farooqch11@gmail.com>
2017-06-08 04:29:34 +0500

    Based off of happy_seed: base, devise, roles, devise_invitable included

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate
+
+  def authenticate
+    unless ENV['HTTP_AUTH_USERNAME'].blank? or ENV['HTTP_AUTH_PASSWORD'].blank?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_AUTH_USERNAME'] && password == ENV['HTTP_AUTH_PASSWORD']
+      end
+    end
+  end
+
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
+
+end
---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module JoaanaCleaningService
+  class Application < Rails::Application
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.generators do |g|
+      g.stylesheets = false
+      g.scaffold_controller "scaffold_controller"
+      g.test_framework :rspec, fixture: true, fixture_replacement: :factory_girl, helper_specs: false, view_specs: false, routing_specs: false, controller_specs: false
+    end
+  
+  end
+end
