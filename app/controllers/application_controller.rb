8ee9a50bc7c828cce0243e3c93443150a8c34fb1
Farooq CH <farooqch11@gmail.com>
2017-06-08 23:58:00 +0500

    basic struture and initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate
 
+  layout :layout_by_resource
+
   def authenticate
     unless ENV['HTTP_AUTH_USERNAME'].blank? or ENV['HTTP_AUTH_PASSWORD'].blank?
       authenticate_or_request_with_http_basic do |username, password|
@@ -13,6 +15,13 @@ class ApplicationController < ActionController::Base
 
   def access_denied(exception)
     redirect_to root_path, alert: exception.message
+  end
+  def layout_by_resource
+    devise_controller? ? "devise" : false
+  end
+
+  def after_sign_in_path_for(resource)
+    edit_user_registration_path
   end
 
 end
