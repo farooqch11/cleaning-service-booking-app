7ed4cb24fae67b57346a66623cb8d7010e93ee6e
Farooq Ch <farooqch11@gmail.com>
2017-06-11 08:55:39 +0500

    employee creation

--- app/controllers/backend/admin/employees_controller.rb
+++ app/controllers/backend/admin/employees_controller.rb
@@ -5,10 +5,24 @@ class Backend::Admin::EmployeesController < Backend::Admin::AdminsController
   end
 
   def new
-    @employee = User.new
+    @employee = User.new(type: 'Employee')
   end
 
   def create
-
+    @employee =  Employee.invite!(employee_params , current_user)
+    if @employee.errors.any?
+      flash[:errors] = @employee.errors.full_messages
+      render 'new'
+    else
+      flash[:success] = "You have successfully created an employee."
+      redirect_to admin_employees_url
+    end
   end
+
+  private
+
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name , :email)
+
+    end
 end
