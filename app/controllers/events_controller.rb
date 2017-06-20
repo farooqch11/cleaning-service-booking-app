002c4b6a6db20af3bf1a7f5e2aef19daf3600708
Farooq CH <farooqch11@gmail.com>
2017-06-21 03:15:29 +0500

    Evente modal closing issue resolved

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,47 +1,40 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html, :json, :js
-
   def index
-    @events = Event.all
-  end 
+    @events = Event.where(start: params[:start]..params[:end])
+  end
 
   def show
-  end 
+  end
 
-  def new 
+  def new
     @event = Event.new
-  end 
+  end
 
   def edit
-  end 
+  end
 
   def create
     @event = Event.new(event_params)
     @event.save
-    respond_with(@event)
-  end 
+  end
 
   def update
     @event.update(event_params)
-    flash[:notice] = 'Event was successfully updated.'
-    respond_with(@event)
-  end 
+  end
 
   def destroy
     @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
-  end 
+  end
 
   private
-    def set_event
-      @event = Event.find(params[:id])
-    end 
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
-    def event_params
-      params.require(:event).permit(:title , :date_range , :start , :end , :color)
-
-    end 
+  def event_params
+    params.require(:event).permit(:title, :date_range, :start, :end, :color , :customer_id , :employee_id)
+  end
 end
  
