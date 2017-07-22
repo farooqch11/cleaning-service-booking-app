c124cb7c1546474a4f111afb153662cb3e648347
Farooq CH <farooqch11@gmail.com>
2017-07-22 20:37:38 +0500

    setup gmail for sending mails on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,15 @@ end
   config.assets.quiet = true
 
   config.action_mailer.default_url_options = { :host => ENV['DOMAIN_URL'] }
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-      api_key: ENV["MAILGUN_API_KEY"],
-      domain: ENV["MAILGUN_DOMAIN"]
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => ENV["EMAIL_ID"],
+      :password =>  ENV["EMAIL_PASSWORD"],
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
 
 end
