1a21823226378b41eb98d776563ae114bbcd461c
farooq ch <farooqch11@gmail.com>
2018-01-04 00:52:29 +0500

    removed migration

--- db/migrate/20180103192746_seed_admin_data.rb
+++
@@ -1,5 +0,0 @@
-class SeedAdminData < ActiveRecord::Migration[5.0]
-  def change
-    Admin.find_or_create_by(email:'admin@joannacleaning.com',password:'password',password_confirmation:'password',role:'admin')
-  end
-end
