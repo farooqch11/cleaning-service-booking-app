4bc92b24ace1504d5e834d30fab4b0043cacb0c7
Usman Ah <usman@asarstrategies.com>
2018-01-31 03:32:23 +0500

    print invoice

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -49,92 +49,6 @@ $(document).ready(function() {
     /* Activating Best In Place */
     jQuery(".best_in_place").best_in_place();
 });
-$("form.validate").each(function(i, el)
-{
-    var $this = $(el),
-        opts = {
-            rules: {},
-            messages: {},
-            errorElement: 'span',
-            errorClass: 'validate-has-error',
-            highlight: function (element) {
-                $(element).closest('.form-group').addClass('validate-has-error');
-            },
-            unhighlight: function (element) {
-                $(element).closest('.form-group').removeClass('validate-has-error');
-            },
-            errorPlacement: function (error, element)
-            {
-                if(element.closest('.has-switch').length)
-                {
-                    error.insertAfter(element.closest('.has-switch'));
-                }
-                else
-                if(element.parent('.checkbox, .radio').length || element.parent('.input-group').length)
-                {
-                    error.insertAfter(element.parent());
-                }
-                else
-                {
-                    error.insertAfter(element);
-                }
-            }
-        },
-        $fields = $this.find('[data-validate]');
-
-
-    $fields.each(function(j, el2)
-    {
-        var $field = $(el2),
-            name = $field.attr('name'),
-            validate = attrDefault($field, 'validate', '').toString(),
-            _validate = validate.split(',');
-
-        for(var k in _validate)
-        {
-            var rule = _validate[k],
-                params,
-                message;
-
-            if(typeof opts['rules'][name] == 'undefined')
-            {
-                opts['rules'][name] = {};
-                opts['messages'][name] = {};
-            }
-
-            if($.inArray(rule, ['required', 'url', 'email', 'number', 'date', 'creditcard']) != -1)
-            {
-                opts['rules'][name][rule] = true;
-
-                message = $field.data('message-' + rule);
-
-                if(message)
-                {
-                    opts['messages'][name][rule] = message;
-                }
-            }
-            // Parameter Value (#1 parameter)
-            else
-            if(params = rule.match(/(\w+)\[(.*?)\]/i))
-            {
-                if($.inArray(params[1], ['min', 'max', 'minlength', 'maxlength', 'equalTo']) != -1)
-                {
-                    opts['rules'][name][params[1]] = params[2];
-
-
-                    message = $field.data('message-' + params[1]);
-
-                    if(message)
-                    {
-                        opts['messages'][name][params[1]] = message;
-                    }
-                }
-            }
-        }
-    });
-
-    $this.validate(opts);
-});
 $(document).ready(function(){
 
     $(document).bind('ajaxError', 'form.ajax', function(event, jqxhr, settings, exception){
@@ -146,6 +60,18 @@ $(document).ready(function(){
     });
 
 });
+
+function PrintElem(id)
+{
+    var printContents = document.getElementById(id).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
 
 (function($) {
 
