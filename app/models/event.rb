2463991138d8f79dfc3bbdd77f332a5e8cb7c008
Farooq CH <farooqch11@gmail.com>
2018-02-09 16:59:47 +0500

    set end date for schedule events

--- app/models/event.rb
+++ app/models/event.rb
@@ -149,7 +149,7 @@ class Event < ApplicationRecord
     end
 
     def set_recurring_end_date
-      self.recurring_end_at = after? ? schedule(start).first(recurring_end_time.to_i).last : start + 3.month
+      self.recurring_end_at = after? ? schedule(start).first(recurring_end_time.to_i).last : start + 2.year
     end
 
     def set_future_events
